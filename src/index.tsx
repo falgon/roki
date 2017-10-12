d615871464d46ab791ce21f197361ddb7c6d0bd3
falgon <falgon53@yahoo.co.jp>
2017-10-12 13:04:34 +0900

    set travis

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,6 @@ import * as ReactDom from 'react-dom';
 import Test from './component/testcomp';
 require('../doc/style/style.scss');
 
-/*interface TestState {
-    rendernize: boolean;
-}
-
-class Test extends React.Component<undefined, TestState> {
-    constructor(props: undefined) {
-	super(props);
-	this.state = {
-	    rendernize: false
-	};
-    }
-
-    public render() {
-	return (<div>
-	    <p>hogehoge</p>
-	</div>);
-    }
-}*/
-
 ReactDom.render(
     <Test message={"hello"} />,
     document.getElementById('content')
