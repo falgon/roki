c3d51878f315ef44c51684271433e812519ea98e
falgon <falgon53@yahoo.co.jp>
2017-10-12 14:05:35 +0900

    initial commit

--- devtools/yml2json.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-require 'yaml'
-require 'json'
-puts JSON.pretty_generate YAML.load ARGF.read
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import Test from './component/testcomp';
-require('../doc/style/style.scss');
-
-ReactDom.render(
-    <Test message={"hello"} />,
-    document.getElementById('content')
-);
