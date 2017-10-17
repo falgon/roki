430df7b0cd147784d24f2b616a6a1ae400d0b30a
falgon <falgon53@yahoo.co.jp>
2017-10-17 12:22:29 +0900

    add doc and assets

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import Main from './component/main';
-import '../docs/style/style.scss';
-
-render(
-    <Main />,
-    document.getElementById('content')
-);
